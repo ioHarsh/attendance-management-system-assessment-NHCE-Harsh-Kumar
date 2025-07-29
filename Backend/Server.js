d1de24818a03bbc4c75ec9e711966997f551a8fd
ioHarsh <h.k.pandey1989@gmail.com>
2025-07-29 15:53:48 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const express=require('express');
+const dotenv=require('dotenv');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
 dotenv.config(); 
-const app = express();
-const port = process.env.PORT || 5000;
-
+const app=express();
+const port=process.env.PORT || 5000;
 app.use(express.json()); 
 const users = [
     { username: 'admin', password: '$2a$10$Q/lc/JYXrfIZf7vOECxO8OoDndpt67n1/Ut5wZkUJ8pTQUHNh.9yO' }, // password is 'admin123'
